77a22b565c4198131810948bbfef1732544e29c2
Vipul Gupta <vipulgupta92998@gmail.com>
2026-07-01 00:32:37 +0530

    otp generating functin

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -3,6 +3,8 @@ import bcryptjs from "bcryptjs"
 import orders from "../schemas/orders.schema.js";
 import positions from "../schemas/positions.schema.js";
 import watchlist from "../schemas/watchlist.schema.js";
+import { redis } from "../index.js";
+import nodemailer from "nodemailer"
 
 export const signup = async (req, res) => {
     try {
@@ -39,13 +41,13 @@ export const signup = async (req, res) => {
 
 export const login = async (req, res) => {
     try {
-        const { email, isPass , password } = req.body;
+        const { email, isPass, password } = req.body;
         const isExist = await user.findOne({ email })
         if (!isExist) {
             console.log("User not found")
             return res.status(400).json({ message: "User not found" })
         }
-        if (isPass == true){
+        if (isPass == true) {
             const isMactch = await bcryptjs.compare(password, isExist.password);
             if (!isMactch) {
                 console.log("Wrong password")
@@ -59,11 +61,39 @@ export const login = async (req, res) => {
     }
 }
 
-export const changePassword = async (req,res)=>{
+const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: process.env.EMAIL_USER, // Reads from .env
+        pass: process.env.EMAIL_PASS  // Reads from .env
+    }
+})
+
+export const otp_for_reset_password = async (req, res) => {
     try {
-         const otp = Math.floor(100000 + Math.random() * 900000).toString()
+        const { email } = req.body;
+        const isExist = await user.findOne({ email })
+        if (!isExist) {
+            return res.status(400).json({ message: "User not signuped" })
+        }
+        const otp = Math.floor(100000 + Math.random() * 900000).toString();
+        await redis.set(`otp:${email}`, otp, "EX", 600)
+        const message="Use this OTP to reset passsword.. This OTP will expire in 10min";
+        try {
+            await transporter.sendMail({
+                to:email,
+                subject:"OTP for reseting password",
+                html:message
+            })
+            return res.status(200).json({message:"OTP sent to registerd email"})
+        } catch (error) {
+            console.log("Error at internal tryCatch of change Password")
+            console.log(error)
+        }
     } catch (error) {
-        
+        console.log("Error at otp for password chnaging")
+        console.log(error)
+        return res.status(500).json({message:"Internal Server error",error})
     }
 }
 
